72c98d688da5d12b5a4935710782cdfde0ac9432
Hack Reactor Student <student@hackreactor.com>
2018-06-29 11:34:10 -0700

    Implement component structure

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,8 @@
 angular.module('video-player')
 
-.component('app', {
-  // TODO
-});
+  .component('app', {
+    bindings: {
+      // name: '@'
+    },
+    templateUrl: './src/templates/app.html',
+  });
--- src/components/search.js
+++ src/components/search.js
@@ -1,5 +1,7 @@
 angular.module('video-player')
-
-.component('search', {
-  // TODO
-});
+  .component('search', {
+    bindings: {
+      // name: '@'
+    },
+    templateUrl: './src/templates/search.html',
+  });
